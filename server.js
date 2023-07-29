3104124dd5c309d89bdfc9b4042d766a7ab7a990
stevy <stevyralambomanana@gmail.com>
2023-07-29 14:18:24 +0200

    env delete

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const {sequelize} = require('./models')
 const cookieParser = require("cookie-parser");
 // const userRoutes = require("./routes/user.routes");
 // const postRoutes = require("./routes/post.routes");
-require("dotenv").config({ path: ".env" });
+require("dotenv").config({ path: "./config/.env" });
 // require("./config/db");
 // const { checkUser, requireAuth } = require("./middleware/auth.middleware");
 const cors = require("cors");
