8fce198c63d7f42ccc15d420421ebc1dd258bcb6
stevy <stevyralambomanana@gmail.com>
2023-08-02 23:22:24 +0200

    test

--- server.js
+++ server.js
@@ -30,13 +30,13 @@ const corsOptions = {
   // maxAge: 3600
 };
 app.use(corsHandler(corsOptions));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://chat-vue-nine.vercel.app"); // Vous pouvez remplacer "*" par les origines autorisées si vous voulez limiter l'accès à certaines origines.
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Spécifiez les méthodes HTTP que vous voulez autoriser ici.
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "https://chat-vue-nine.vercel.app"); // Vous pouvez remplacer "*" par les origines autorisées si vous voulez limiter l'accès à certaines origines.
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // Spécifiez les méthodes HTTP que vous voulez autoriser ici.
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   next();
+// });
 
 app.use(cookieParser());
 
